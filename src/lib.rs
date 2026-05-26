1136b291c971f9064bfb8a16d8785b127551a281
Julian Knodt <julian.knodt@cephia.ai>
2026-05-26 17:15:16 -0400

    Add bilateral u16

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ pub use bilateral::*;
 mod bilateral_f32;
 pub use bilateral_f32::*;
 
+mod bilateral_u16;
+pub use bilateral_u16::bilateral_filter as bilateral_filter_u16;
+
 mod guided_upsample;
 pub use guided_upsample::*;
 
@@ -52,11 +55,24 @@ pub fn diff_factor<const C: usize>(a: [u8; C], b: [u8; C]) -> u8 {
 }
 
 pub fn diff_factor_f32<const C: usize>(a: [f32; C], b: [f32; C]) -> f32 {
-  let mut sum = 0.;
-  for i in 0..C {
-    sum += (a[i] - b[i]).abs()
-  }
-  sum
+    let mut sum = 0.;
+    for i in 0..C {
+        sum += (a[i] - b[i]).abs()
+    }
+    sum
+}
+
+pub fn diff_factor_u16<const C: usize>(a: [u16; C], b: [u16; C]) -> u16 {
+    match C {
+        1 => a[0].abs_diff(b[0]),
+        3 => {
+            let r = a[0].abs_diff(b[0]) >> 2;
+            let g = a[1].abs_diff(b[1]) >> 1;
+            let b = a[2].abs_diff(b[2]) >> 2;
+            r + g + b
+        }
+        c => todo!("Not implemented for {c} channels"),
+    }
 }
 
 #[inline]
